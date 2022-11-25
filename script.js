fe3fdcc40c0a6185c33277bf4aae2a0eca483314
Ryan <ravalles10@gmail.com>
2022-11-25 16:55:04 +0900

    Add basic mathematic functions

---
+++ script.js
@@ -0,0 +1,21 @@
+//make a button that adds
+function add(a, b){
+    a + b;
+};
+//a button that subtracts
+function subtract(a, b){
+dif = a - b;
+return dif
+}
+//a button that multiplies
+function multiply(a, b){
+  product = a * b;
+    return product
+}
+//a button that divides
+function divide(a, b){
+   quotient = a/b;
+   return quotient
+}
+
+//create an equal button
