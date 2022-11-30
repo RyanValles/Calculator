630f392c1db78a871fce4b842f460cf088cf990d
Ryan <ravalles10@gmail.com>
2022-11-30 13:10:58 +0900

    Add solution and decimal function

--- script.js
+++ script.js
@@ -8,22 +8,26 @@ const equal = document.querySelector('#equals')
 function add(num1, num2){
   let res = num1 + num2;
    return res
-};
+}
+
 //a button that subtracts
 function subtract(num1, num2){
 dif = num1 - num2;
 return dif
 }
+
 //a button that multiplies
 function multiply (num1, num2){
   product = num1 * num2;
     return product
 }
+
 //a button that divides
 function divide(num1, num2){
    quotient = num1/num2;
    return quotient
 }
+
 let num1 = ''
 let num2 = ''
 let operater = ''
@@ -41,18 +45,22 @@ function operate(operater, num1, num2){
 }
   else (operater === divide)
    return divide(num1, num2); 
-
 }
 
-
-
-// display makes what button is clicked
+// display makes what number is clicked
 const screen = document.getElementById('display');
 function displayNum(x){
   (screen.innerHTML += x).toString()
 }
 
-//del deletes one thing
+//adds decimal
+function decimal(){
+  if (!screen.innerHTML.includes('.') ){
+  return (screen.innerHTML += '.');
+  }
+}
+
+//del deletes last item
 const del = document.getElementById('delete');
 function delt(){
   screen.textContent = screen.textContent.slice(0, -1)
@@ -64,21 +72,22 @@ function clearAll(){
   screen.innerHTML = ''
 }
 
-
 //when operator is clicked let num1 = screen.innerhtml
-// and let operator = the button clicked
+// and let operator = the operator clicked
 //then clear display
 function Oper(x){
  num1 = (+screen.textContent);
  operater = x;
 screen.innerHTML = '';
- 
 }
+
 //store num2 then call operate function and display the return
-function eqal(){
+function solution(){
   num2 = (+screen.textContent);
  screen.innerHTML = '';
  screen.innerHTML = operate(operater, num1, num2);
-  
+ num2 = ''
+ num1 = (+screen.textContent);
+ operater = ''
  }
  
